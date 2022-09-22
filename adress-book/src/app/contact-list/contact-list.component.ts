4f3c475db5bf0f33bfddc737bc76b6e432f4eeb5
atn362 <atn362@gmail.com>
2022-09-22 16:02:58 -0500

    inin

---
+++ adress-book/src/app/contact-list/contact-list.component.ts
@@ -0,0 +1,23 @@
+import {Component, Input, OnInit} from '@angular/core';
+import {IContact} from "../interfaces/IContact";
+
+@Component({
+  selector: 'app-contact-list',
+  templateUrl: './contact-list.component.html',
+  styleUrls: ['./contact-list.component.css']
+})
+export class ContactListComponent implements OnInit {
+
+  @Input() list!: IContact[];
+
+  constructor() {
+
+  }
+
+  ngOnInit(): void {
+
+  }
+
+
+
+}
---
+++ adress-book/src/app/interfaces/IContact.ts
@@ -0,0 +1,12 @@
+export interface IContact {
+  id: number
+  name: string
+  address: string
+  phoneNumber: string
+  email: string
+  // birthday: Date
+  // meetingDate: Date
+  // company: string
+  // note: string
+
+}
