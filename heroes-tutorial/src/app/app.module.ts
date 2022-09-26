116d23d436c0ccceabc94751d3501fc07560cecc
atn362 <atn362@gmail.com>
2022-09-26 16:36:51 -0500

    changes to ts and html on heroes component

--- heroes-tutorial/src/app/app.module.ts
+++ heroes-tutorial/src/app/app.module.ts
@@ -2,17 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { HerosComponent } from './heros/heros.component';
 import { HeroesComponent } from './heroes/heroes.component';
+import { FormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
     AppComponent,
-    HerosComponent,
     HeroesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
