20f32af1d388714959acb2a8efe37002b4a3b3ca
atn362 <atn362@gmail.com>
2022-09-26 15:51:00 -0500

    heroes componenet generated

--- heroes-tutorial/src/app/app.module.ts
+++ heroes-tutorial/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HerosComponent } from './heros/heros.component';
+import { HeroesComponent } from './heroes/heroes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HerosComponent,
+    HeroesComponent
   ],
   imports: [
     BrowserModule
