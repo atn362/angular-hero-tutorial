4786d082d8727ad473bf28e23128ee7adde904c6
atn362 <atn362@gmail.com>
2022-09-27 13:00:17 -0500

    changes to ts and html on heroes component

--- heroes-tutorial/src/app/mock-heroes.ts
+++ heroes-tutorial/src/app/mock-heroes.ts
@@ -1,13 +1,13 @@
 import { Hero } from './hero';
 
 export const HEROES: Hero[] = [
-  { id: 12, name: 'Dr. Nice' },
-  { id: 13, name: 'Bombasto' },
-  { id: 14, name: 'Celeritas' },
-  { id: 15, name: 'Magneta' },
-  { id: 16, name: 'RubberMan' },
-  { id: 17, name: 'Dynama' },
-  { id: 18, name: 'Dr. IQ' },
-  { id: 19, name: 'Magma' },
-  { id: 20, name: 'Tornado' }
+  { id: 12, name: 'Dr. Nice', powers: 'super nice' },
+  { id: 13, name: 'Bombasto', powers: 'explosives expert' },
+  { id: 14, name: 'Celeritas', powers: 'controls sun rays' },
+  { id: 15, name: 'Magneta', powers: 'controls ferrous items' },
+  { id: 16, name: 'RubberMan', powers: 'stretchy appendages' },
+  { id: 17, name: 'Dynama', powers: 'controls electricity' },
+  { id: 18, name: 'Dr. IQ', powers: 'extreme intellect' },
+  { id: 19, name: 'Magma', powers :'controls lava' },
+  { id: 20, name: 'Tornado', powers: 'controls wind' }
 ];
