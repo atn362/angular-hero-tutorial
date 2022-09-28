674a0e57fe46dddd307e5759d5e0028be63f37d9
atn362 <atn362@gmail.com>
2022-09-28 16:31:40 -0500

    changes

--- water-calc/src/app/enums/ACTIVITY_LEVEL.ts
+++
@@ -1,5 +0,0 @@
-export enum ACTIVITY_LEVEL {
-  LOW = "LOW",
-  AVERAGE = "AVERAGE",
-  HIGH = "HIGH",
-}
--- water-calc/src/app/interfaces/IProfile.ts
+++
@@ -1,8 +0,0 @@
-import {ACTIVITY_LEVEL} from "../enums/ACTIVITY_LEVEL";
-
-export interface IProfile {
-  name: string
-  weight_lbs: number
-  activity_level: ACTIVITY_LEVEL
-
-}
